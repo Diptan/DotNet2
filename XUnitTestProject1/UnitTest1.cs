049a2d464a14ce18d698659cad0ca8039ca95d72
Mykhailo Dyptan <mykhailo.dyptan@insidemedia.onmicrosoft.com>
2019-05-07 13:23:34 +0300

    tet

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -18,7 +18,7 @@ namespace XUnitTestProject1
         {
             var capabilities = new DesiredCapabilities("chrome", "73.0", new Platform(PlatformType.Any));
             capabilities.SetCapability("enableVNC", true);
-            var ff = new RemoteWebDriver(new Uri("http://10.17.8.70:4444/wd/hub"), capabilities);
+            var ff = new RemoteWebDriver(new Uri("http://10.17.11.107:4444/wd/hub"), capabilities);
 
             ff.Navigate().GoToUrl("https://www.google.com");
             Thread.Sleep(10000);
