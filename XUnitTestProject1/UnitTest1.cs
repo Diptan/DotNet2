9efd9757720419cf7fe38ac7091562f3b6278b8e
Mykhailo Dyptan <mykhailo.dyptan@insidemedia.onmicrosoft.com>
2019-05-07 14:26:46 +0300

    tet

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -28,88 +28,90 @@ namespace XUnitTestProject1
             Console.WriteLine("Step 2");
             Assert.AreEqual(1, 1);
         }
+
+
+
+        [Test]
+        public void Test12()
+        {
+            var capabilities = new DesiredCapabilities("chrome", "73.0", new Platform(PlatformType.Any));
+            capabilities.SetCapability("enableVNC", true);
+            var ff = new RemoteWebDriver(new Uri("http://10.17.8.70:4444/wd/hub"), capabilities);
+
+            ff.Navigate().GoToUrl("https://www.google.com");
+            ff.Quit();
+            Console.WriteLine("Step 1");
+            Console.WriteLine("Step 2");
+            Assert.AreEqual(1, 1);
+        }
+    }
+
+
+    [Parallelizable]
+    [TestFixture]
+    public class UnitTest2
+    {
+        [Test]
+        public void Test21()
+        {
+            var capabilities = new DesiredCapabilities("chrome", "73.0", new Platform(PlatformType.Any));
+            capabilities.SetCapability("enableVNC", true);
+            var ff = new RemoteWebDriver(new Uri("http://10.17.8.70:4444/wd/hub"), capabilities);
+
+            ff.Navigate().GoToUrl("https://www.google.com");
+            ff.Quit();
+            Console.WriteLine("Step 1");
+            Console.WriteLine("Step 2");
+            Assert.AreEqual(1, 1);
+        }
+
+        [Test]
+        public void Test22()
+        {
+            var capabilities = new DesiredCapabilities("chrome", "73.0", new Platform(PlatformType.Any));
+            capabilities.SetCapability("enableVNC", true);
+            var ff = new RemoteWebDriver(new Uri("http://10.17.8.70:4444/wd/hub"), capabilities);
+
+            ff.Navigate().GoToUrl("https://www.google.com");
+            ff.Quit();
+            Console.WriteLine("Step 1");
+            Console.WriteLine("Step 2");
+            Assert.AreEqual(1, 1);
+        }
+    }
+
+    [Parallelizable]
+    [TestFixture]
+    public class UnitTest3
+    {
+        [Test]
+        public void Test31()
+        {
+            var capabilities = new DesiredCapabilities("chrome", "73.0", new Platform(PlatformType.Any));
+            capabilities.SetCapability("enableVNC", true);
+            var ff = new RemoteWebDriver(new Uri("http://10.17.8.70:4444/wd/hub"), capabilities);
+
+            ff.Navigate().GoToUrl("https://www.google.com");
+            ff.Quit();
+            Console.WriteLine("Step 1");
+            Console.WriteLine("Step 2");
+            Assert.AreEqual(1, 2);
+        }
+
+        [Test]
+        public void Test32()
+        {
+            var capabilities = new DesiredCapabilities("chrome", "73.0", new Platform(PlatformType.Any));
+            capabilities.SetCapability("enableVNC", true);
+            var ff = new RemoteWebDriver(new Uri("http://10.17.8.70:4444/wd/hub"), capabilities);
+
+            ff.Navigate().GoToUrl("https://www.google.com");
+            ff.Quit();
+            Console.WriteLine("Step 1");
+            Console.WriteLine("Step 2");
+            Assert.AreEqual(1, 1);
+        }
     }
 }
-
-//    [Test]
-    //    public void Test12()
-    //    {
-    //        var capabilities = new DesiredCapabilities("chrome", "73.0", new Platform(PlatformType.Any));
-    //        capabilities.SetCapability("enableVNC", true);
-    //        var ff = new RemoteWebDriver(new Uri("http://10.17.8.70:4444/wd/hub"), capabilities);
-
-    //        ff.Navigate().GoToUrl("https://www.google.com");
-    //        ff.Quit();
-    //        Console.WriteLine("Step 1");
-    //        Console.WriteLine("Step 2");
-    //        Assert.AreEqual(1, 1);
-    //    }
-
-    //}
-    //[Parallelizable]
-    //[TestFixture]
-    //public class UnitTest2
-    //{
-    //    [Test]
-    //    public void Test21()
-    //    {
-    //        var capabilities = new DesiredCapabilities("chrome", "73.0", new Platform(PlatformType.Any));
-    //        capabilities.SetCapability("enableVNC", true);
-    //        var ff = new RemoteWebDriver(new Uri("http://10.17.8.70:4444/wd/hub"), capabilities);
-
-    //        ff.Navigate().GoToUrl("https://www.google.com");
-    //        ff.Quit();
-    //        Console.WriteLine("Step 1");
-    //        Console.WriteLine("Step 2");
-    //        Assert.AreEqual(1, 1);
-    //    }
-
-    //    [Test]
-    //    public void Test22()
-    //    {
-    //        var capabilities = new DesiredCapabilities("chrome", "73.0", new Platform(PlatformType.Any));
-    //        capabilities.SetCapability("enableVNC", true);
-    //        var ff = new RemoteWebDriver(new Uri("http://10.17.8.70:4444/wd/hub"), capabilities);
-
-    //        ff.Navigate().GoToUrl("https://www.google.com");
-    //        ff.Quit();
-    //        Console.WriteLine("Step 1");
-    //        Console.WriteLine("Step 2");
-    //        Assert.AreEqual(1, 1);
-    //    }
-    //}
-
-    //[Parallelizable]
-    //[TestFixture]
-    //public class UnitTest3
-    //{
-    //    [Test]
-    //    public void Test31()
-    //    {
-    //        var capabilities = new DesiredCapabilities("chrome", "73.0", new Platform(PlatformType.Any));
-    //        capabilities.SetCapability("enableVNC", true);
-    //        var ff = new RemoteWebDriver(new Uri("http://10.17.8.70:4444/wd/hub"), capabilities);
-
-    //        ff.Navigate().GoToUrl("https://www.google.com");
-    //        ff.Quit();
-    //        Console.WriteLine("Step 1");
-    //        Console.WriteLine("Step 2");
-    //        Assert.AreEqual(1, 1);
-    //    }
-
-    //    [Test]
-    //    public void Test32()
-    //    {
-    //        var capabilities = new DesiredCapabilities("chrome", "73.0", new Platform(PlatformType.Any));
-    //        capabilities.SetCapability("enableVNC", true);
-    //        var ff = new RemoteWebDriver(new Uri("http://10.17.8.70:4444/wd/hub"), capabilities);
-
-    //        ff.Navigate().GoToUrl("https://www.google.com");
-    //        ff.Quit();
-    //        Console.WriteLine("Step 1");
-    //        Console.WriteLine("Step 2");
-    //        Assert.AreEqual(1, 1);
-    //    }
-    //}
 
 
